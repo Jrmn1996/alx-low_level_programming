e5e0a8e17f57963a904e1a2618d01eba80760f00
Jrmn1996 <jrmn1996@gmail.com>
2023-03-24 08:11:27 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangles
+ * @size: parameter to be checked
+ * Return: always 0
+ */
+void print_triangle(int size)
+{
+	int h;
+	int b;
+
+	if (size > 0)
+	{
+		for (h = 1 ; h <= size ; h++)
+		{
+			for (b = 1 ; b <= size ; b++)
+			{
+				if ((h + b) <= size)
+				{
+					_putchar(32);
+				}
+				else
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
