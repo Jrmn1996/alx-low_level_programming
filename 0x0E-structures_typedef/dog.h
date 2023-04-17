2657dae291e10a55ed6ecbf3255955786264d86d
Jrmn1996 <jrmn1996@gmail.com>
2023-04-17 15:06:19 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner name
+ * Description: dog ID
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /*DOG_H*/
