baa54816adc2d914dd61ca179641771db1443cfd
Jrmn1996 <jrmn1996@gmail.com>
2023-04-27 16:50:58 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_list - frees all nodes of a list
+ * @head: pointer to the head node
+ * Return: always 0
+ */
+void free_list(list_t *head)
+{
+	list_t *node;
+	list_t *next_node;
+
+	if (!head)
+	{
+		return;
+	}
+	node = head;
+	while (node)
+	{
+		next_node = node->next;
+		free(node->str);
+		free(node);
+		node->next_node;
+	}
+}
