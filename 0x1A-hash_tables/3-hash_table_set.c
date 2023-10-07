e69e745e7a15d654a985560c0fb7bbe874a3fbe5
Jrmn1996 <jrmn1996@gmail.com>
2023-10-06 18:47:39 -0700

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -7,7 +7,7 @@
  */
 hash_node_t *make_hash_node(const char *key, const char *value)
 {
-	hash_node_t *nodde;
+	hash_node_t *node;
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
@@ -51,9 +51,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(temp->key, key) == 0)
 		{
 			new_value = strdup(value);
-			if (new_value == NILL)
+			if (new_value == NULL)
 				return (0);
-			free(temp->valur);
+			free(temp->value);
 			temp->value = new_value;
 			return (1);
 		}
